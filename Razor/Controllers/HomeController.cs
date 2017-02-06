81dd48307416c521cf2e80307d4437a0c3f04207
Charles Kuo <chao.kuo@gmail.com>
2017-02-05 23:31:23 -0800

    Add project files.

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Razor.Models;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index() {
+            return View(myProduct);
+        }
+    }
+}
